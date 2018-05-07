0fc191dcd2333455d3dbb5877d06cacc04bc84bc
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2018-05-07 19:34:41 +0500

    Add exercise + drill 28

---
+++ drills/drill_28_1.py
@@ -0,0 +1,30 @@
+"""This drill demonstrates some of the comparison operators found in Python"""
+
+print("\nThe equality operator ==")
+print("5 == 5?", 5 == 5)
+
+print("\nThe inequality operator !=")
+print("5 != 5?", 5 != 5)
+
+print("\nThe greater than operator >")
+print("6 > 5?", 6 > 5)
+
+print("\nThe less than operator <")
+print("6 < 5?", 6 < 5)
+
+print("\nThe greater-than-equal-to operator >=")
+print("5 >= 5?", 5 >= 5)
+print("6 >= 5?", 6 >= 5)
+
+print("\nThe less-than-equal-to operator <=")
+print("5 <= 5?", 5 <= 5)
+print("6 <= 5?", 6 <= 5)
+
+print("\nLess known, but sort of useful - 'is not'")
+print("'Saad' is not 'saad'?", 'Saad' is not 'saad')
+
+print("\nAlso less known, but useful - 'is'.")
+print("'Saad' is 'Saad?", 'Saad' is 'Saad')
+
+print("\nOther operators are present, but they're not equality operators.")
+print("Bitwise operators, membership operators.")
